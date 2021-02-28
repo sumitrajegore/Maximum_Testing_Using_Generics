0b1d5c5ff885d2b0ec6697d16520190df46d62ba
sumitrajegore <sumitrajegorevp@gmail.com>
2021-02-28 16:35:50 +0530

    find maximum of three String

--- MaximumCheck.java
+++ MaximumCheck.java
@@ -2,11 +2,14 @@ package generics;
 
 public class MaximumCheck {
 
-	public Float maximumValue(Float d, Float e, Float f) {
-		Float max = d;
+	public String maximumValue(String d, String e, String f) {
+		
+		String max = d;
 		if (e.compareTo(max) > 0) {
+			
 			max = e;
 		} if (f.compareTo(max) > 0) {
+			
 			max = f;
 		} 
 		System.out.println(max +" is Maximum");

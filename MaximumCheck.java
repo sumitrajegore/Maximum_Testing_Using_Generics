decdf82dfce8112de7724a945b7b0a8bcb1b82b5
sumitrajegore <sumitrajegorevp@gmail.com>
2021-02-28 15:36:04 +0530

    find maximum of three float numbers

--- MaximumCheck.java
+++ MaximumCheck.java
@@ -1,15 +1,13 @@
 package generics;
 
-import java.awt.print.Printable;
-
 public class MaximumCheck {
 
-	public Integer maximumValue(Integer x, Integer y, Integer z) {
-		Integer max = x;
-		if (y.compareTo(max) > 0) {
-			max = y;
-		} if (z.compareTo(max) > 0) {
-			max = z;
+	public Float maximumValue(Float d, Float e, Float f) {
+		Float max = d;
+		if (e.compareTo(max) > 0) {
+			max = e;
+		} if (f.compareTo(max) > 0) {
+			max = f;
 		} 
 		System.out.println(max +" is Maximum");
 		return max;
--- MaximumCheckTest.java
+++ MaximumCheckTest.java
@@ -5,26 +5,29 @@ import org.junit.Test;
 public class MaximumCheckTest {
 
 	// test to check if maximum value is at first position.
+	@SuppressWarnings("deprecation")
 	@Test
 	public void MaximumValueAtFirstPosition() {
 		MaximumCheck maximumCheck = new MaximumCheck();
-		int maximum = maximumCheck.maximumValue(15, 12, 14);
-		Assert.assertEquals(15, maximum);
+		float maximum = maximumCheck.maximumValue(15.5f, 12.4f, 14.5f);
+		Assert.assertEquals(15.5f, maximum, 0.001);
 	}
 
 	// test to check if maximum value is at second position.
+	@SuppressWarnings("deprecation")
 	@Test
 	public void MaximumValueAtSecondPosition() {
 		MaximumCheck maximumCheck = new MaximumCheck();
-		int maximum = maximumCheck.maximumValue(12, 15, 14);
-		Assert.assertEquals(15, maximum);
+		float maximum = maximumCheck.maximumValue(12.5f, 15.7f, 14.3f);
+		Assert.assertEquals(15.7f, maximum, 0.001);
 	}
 
 	// test to check if maximum value is at third position.
+	@SuppressWarnings("deprecation")
 	@Test
 	public void MaximumValueAtThirdPosition() {
 		MaximumCheck maximumCheck = new MaximumCheck();
-		int maximum = maximumCheck.maximumValue(12, 14, 15);
-		Assert.assertEquals(15, maximum);
+		float maximum = maximumCheck.maximumValue(12.4f, 14.4f, 15.2f);
+		Assert.assertEquals(15.2f, maximum, 0.001);
 	}
 }
